b3812110ce5b481c8cf390a08777753f0c37521e
Hudson Lima Tavares <hudsonlimatavares@gmail.com>
2023-04-21 23:01:23 -0300

    Criação do Menu Lista de Produtos

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,19 +1,27 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import { SideMenuAdmin } from '../../components'
+import paths from '../../constants/paths'
 import ListProducts from './ListProducts'
 // eslint-disable-next-line no-unused-vars
 import Orders from './Orders'
 import { Container, ContainerItems } from './styles'
 
-export function Admin() {
+export function Admin({ match: { path } }) {
   return (
     <Container>
       <SideMenuAdmin />
       <ContainerItems>
-        {/* <Orders /> */}
-        <ListProducts />
+        {path === paths.Order && <Orders />}
+        {path === paths.Products && <ListProducts />}
       </ContainerItems>
     </Container>
   )
+}
+
+Admin.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string
+  })
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
+import paths from '../constants/paths'
 import { Home, Login, Product, Register, Cart, Admin } from '../containers'
 import PrivateRoute from './private-routes'
 
@@ -13,8 +14,8 @@ function Routes() {
         <PrivateRoute exact component={Home} path="/" />
         <PrivateRoute component={Product} path="/produtos" />
         <PrivateRoute component={Cart} path="/carrinho" />
-        <PrivateRoute component={Admin} path="/pedidos" isAdmin />
-        <PrivateRoute component={Admin} path="/listar-produtos" isAdmin />
+        <PrivateRoute component={Admin} path={paths.Order} isAdmin />
+        <PrivateRoute component={Admin} path={paths.Products} isAdmin />
       </Switch>
     </Router>
   )
